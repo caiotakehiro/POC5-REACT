3237e14e0267fbb1299929eb1f2f0b8fb896c095
caiotakehiro <144562821+caiotakehiro@users.noreply.github.com>
2024-10-25 01:34:10 -0300

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Header from '@components/app/components/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header title="Bom dia, Vietnã!" />
+      <p>Não sei o que botar de subtítulo :P</p>
+    </div>
+  );
+};
+
+export default HomePage;
